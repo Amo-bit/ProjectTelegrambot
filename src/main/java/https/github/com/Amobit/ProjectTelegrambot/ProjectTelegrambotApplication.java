29ad392533d65e0f27f080bccd140857a534f3f4
ilmir <ilmir302@yandex.ru>
2021-11-10 07:33:19 +0500

    Generate SpringBoot

---
+++ src/main/java/https/github/com/Amobit/ProjectTelegrambot/ProjectTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.Amobit.ProjectTelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTelegrambotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/https/github/com/Amobit/ProjectTelegrambot/ProjectTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.Amobit.ProjectTelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
